ebf7e13ab17563c4477d493833778b2a8886e5a1
David Apakin <apakin.david@gmail.com>
2024-07-30 14:40:19 +0400

    Changed project structure in part4

---
+++ part4/bloglist/app.js
@@ -0,0 +1,34 @@
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRoutes = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
+
+mongoose.set('strictQuery', false)
+
+logger.info('connecting to', config.MONGODB_URL)
+
+
+mongoose.connect(config.MONGODB_URL)
+    .then(() => {
+        console.log('connected to MongoDB')
+    })
+    .catch((error) => {
+        console.error('error connecting to MongoDB', error.message)
+    })
+
+
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogsRoutes)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
