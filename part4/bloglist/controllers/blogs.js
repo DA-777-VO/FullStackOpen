40f7b63806aebbf5afb872c207f643d5da1844a1
David Apakin <apakin.david@gmail.com>
2024-10-10 17:45:45 +0400

    Added the ability to get a blog by a specific id.

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -9,6 +9,19 @@ blogsRouter.get('/', async (request, response) => {
   response.json(blogs)
 })
 
+blogsRouter.get('/:id', async (request, response, next) => {
+  try{
+    const blog = await Blog.findById(request.params.id)
+    if (blog) {
+      response.json(blog)
+    } else {
+      response.status(404).end()
+    }
+  } catch (exception) {
+    next(exception)
+  }
+})
+
 blogsRouter.post('/', async (request, response, next ) => {
   const body = request.body
   const user = request.user
