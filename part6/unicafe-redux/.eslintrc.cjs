88e8b35f73eea290244767a769bb212b84fc1d00
David Apakin <apakin.david@gmail.com>
2024-12-08 23:29:08 +0400

    6.1-6.2 added gitignore and other files

---
+++ part6/unicafe-redux/.eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: { browser: true,
+    es2020: true,
+    jest: true,
+    "jest/globals": true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parserOptions: { ecmaVersion: 2020, sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+}
